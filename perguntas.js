80f99bc3ed011dd1e0dc89aec3c3fd72155e67ba
bugraoganhaplmds <00001120552552sp@al.educacao.sp.gov.br>
2024-11-06 13:52:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Que ano o Guarani foi fundado?',
+    '1911'
+)
+
+criaCartao(
+    'Música',
+    'Quem é o vocalista do Charlie Brown Junior?',
+    'Chorão'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual nome do estadio do Guarani?',
+    'Brinco de Ouro da Princesa'
+)
+
+criaCartao(
+    'Música',
+    'Quem é o guitarrista do Queen?',
+    'Brian May'
+)
